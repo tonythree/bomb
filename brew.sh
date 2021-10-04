4c0b88e36754fc0f2de6b10700ec989953d2b3fb
Antonio Tripiana <tonitripiana@gmail.com>
2021-10-04 19:25:24 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -2,6 +2,7 @@
 echo "💣 Brew install"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "💣 Adding brew to your zprofile"
+echo ''
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 echo "💣 Activating brew for the current console session"
 eval "$(/opt/homebrew/bin/brew shellenv)"
