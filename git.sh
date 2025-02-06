737a735aa07bb1ee0c6ef345bacf11cb0f3af765
Antonio Tripiana <tonitripiana@gmail.com>
2025-02-06 15:30:59 +0100

    Update to id_ed25519. Update flags. Following official guide
    Following official github guide at https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account

--- git.sh
+++ git.sh
@@ -2,15 +2,15 @@
 echo "💣 Git init"
 echo "💣 Enter your email"
 read EMAIL
-ssh-keygen -t rsa -b 4096 -C "$EMAIL"
+ssh-keygen -t ed25519 -C "$EMAIL"
 eval "$(ssh-agent -s)"
 touch ~/.ssh/config
 echo "Host *
   AddKeysToAgent yes
   UseKeychain yes
-  IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-ssh-add -K ~/.ssh/id_rsa
-pbcopy < ~/.ssh/id_rsa.pub
+  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+pbcopy < ~/.ssh/id_ed25519.pub
 echo "💣 I copied the public key in your clipboard. You need to create a new Redirecting to Github"
 read -p "Do you want me to open Github? " -n 1 -r
 echo    # (optional) move to a new line
