0911dd728e71e3fb13ac469da75701f37727ac34
Antonio Tripiana <tonitripiana@gmail.com>
2025-02-13 22:53:00 +0100

    Update javascript.sh

--- javascript.sh
+++ javascript.sh
@@ -6,7 +6,7 @@ rm /usr/local/bin/node
 rm /usr/local/share/man/man1/node.1
 rm /usr/local/lib/dtrace/node.d
 echo "💣 Install NVM (Node Version Manager)"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.zshrc
 nvm install stable
 nvm use --delete-prefix stable
